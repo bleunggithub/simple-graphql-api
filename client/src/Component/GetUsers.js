c57ede77533167b770fc47104239a94047711e99
B Leung <leungcheukkei@gmail.com>
2021-04-14 01:59:03 +0800

    add component to display all users in a table, create add user component

--- client/src/Component/GetUsers.js
+++ client/src/Component/GetUsers.js
@@ -1,17 +1,36 @@
-import React, {useEffect} from 'react'
-import { useQuery, gql } from '@apollo/client'
+import React, {useEffect, useState} from 'react'
+import { useQuery } from '@apollo/client'
 import {GET_USERS} from '../GraphQL/Queries'
 
 export default function GetUsers() {
-    const { error, loading, data } = useQuery(GET_USERS)
-    
+    const { data } = useQuery(GET_USERS)
+    const [users, setUsers] = useState([])
+
     useEffect(() => {
-        console.log(data)
+        if(data) setUsers(data.getAllUsers)
     }, [data])
     
     return (
         <div>
-            
+            <h1>All Users</h1>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                </tr>
+                    {users.map(user => (
+                        <tr key={user.id}>
+                            <td>{ user.id }</td>
+                            <td>{ user.first_name }</td>
+                            <td>{ user.last_name }</td>
+                            <td>{ user.email }</td>
+                            <td>{ user.password }</td>
+                        </tr>
+                    ))}
+            </table>
         </div>
     )
 }
